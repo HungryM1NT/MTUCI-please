d5a67606debeb9e2df7e4022026b6dd120ea7fb9
M1NT <ilya.ablyazov.2004@gmail.com>
2023-10-08 16:02:36 +0300

    Backend brach

--- level_selector/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LevelSelectorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'level_selector'
--- level_selector/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-# from users.models import User
-#
-#
-# class LeaderBoard(models.Model):
-#     level = models.IntegerField()
-#     name = models.ForeignKey(to=User, on_delete=models.CASCADE)
-#     score = models.IntegerField()
